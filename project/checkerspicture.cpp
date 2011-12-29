902f35155d080602e9061174e47bc779c6b4b13f
????? <?????@ACER4820TG>
2011-12-30 01:08:17 +0400

    ?????

---
+++ project/checkerspicture.cpp
@@ -0,0 +1,202 @@
+#include "checkerspicture.h"
+
+CheckersPicture::CheckersPicture(QWidget *parent) : QWidget(parent)
+{
+    QPalette palette;
+    palette.setColor(QPalette::Light,QColor(0x87,0xa1,0xc0));
+    setPalette(palette);
+    setBackgroundRole(QPalette::Light);
+    setAutoFillBackground(true);
+
+    setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+    setAttribute(Qt::WA_StaticContents);
+
+    zoom = 32;
+    n = 8;
+    selectedCelli = -1;
+    selectedCellj = -1;
+    mouseClickCount = 0;
+    gameStarted = false;
+    setMinimumSize(zoom*(n+1),zoom*(n+1));
+}
+
+CheckersPicture::~CheckersPicture()
+{
+
+}
+
+void CheckersPicture::setDraughts(CheckerState ** _draughts)
+{
+    this->draughts = _draughts;
+}
+
+void CheckersPicture::mousePressEvent(QMouseEvent *event)
+{
+    if(this->gameStarted == false)
+        return;
+
+    if (event->buttons() && Qt::LeftButton)
+    {
+        qreal i_d = (event->pos().x() - p.x() + side/(2*n+2))*(n+1)/side - 1.0;
+        qreal j_d = (double)n - (event->pos().y() - p.y() + side/(2*n+2))*(n+1)/side;
+        //перемудрено с i и j конечно..
+        int i = n - (int)j_d - 1, j = (int)i_d;
+        qDebug() << "pressed on " << i << " " << j;
+
+        //если опять нажимают на ту же клетку - выходим
+        if(selectedCelli == i && selectedCellj == j)
+            return;
+
+        //если нажато не на белую клетку
+        if((i + j) % 2 == 1)
+        {
+            //и по белой пешке, дамке
+            if(mouseClickCount == 0 && (draughts[i][j] == WHITE || draughts[i][j] == WHITE_KING))
+            {
+                selectedCelli = i;
+                selectedCellj = j;
+                mouseClickCount++;
+                qDebug() << "first";
+                this->update();
+            }
+            else if(mouseClickCount == 1 && draughts[i][j] == NONE)//на пустой клетке
+            {
+                //сигнал о перемещении c selectedcelli, selectedcellj на i, j
+                //enum CheckerState state = this->draughts[selectedCelli][selectedCellj];
+                //this->draughts[selectedCelli][selectedCellj] = NONE;
+                //this->draughts[i][j] = state;
+
+                qDebug() << "second";
+                qDebug() << "emit" << selectedCelli << selectedCellj << i << j;
+                emit playerMove(selectedCelli, selectedCellj, i, j);
+                selectedCelli = -1;
+                selectedCellj = -1;
+                mouseClickCount = 0;
+            }
+        }
+        else
+        {
+            //нажатие на белую клетку
+            selectedCelli = -1;
+            selectedCellj = -1;
+            mouseClickCount = 0;
+            qDebug() << "empty";
+
+            this->update();
+        }
+        /*
+        if(color==BLACK)
+            emit mouseClicked((int)i,(int)j);
+        else
+            emit mouseClicked(n - 1 - (int)i, n - 1 - (int)j);*/
+    }
+}
+
+void CheckersPicture::mouseMoveEvent(QMouseEvent *event)
+{
+
+}
+
+void CheckersPicture::paintEvent(QPaintEvent *event)
+{
+    qDebug() << "CheckersPicture::paintEvent()";
+
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing, true);
+    painter.setViewport(p.x(),p.y(),side,side);
+    painter.setWindow(0, 0, zoom*(n+1.0), zoom*(n+1.0));
+
+    QColor border(0xce,0x5c,0x00);
+    painter.fillRect(QRect(0,0,zoom*(n+1.0),zoom*0.5), border);
+    painter.fillRect(QRect(0,zoom*(n+0.5),zoom*(n+1.0),zoom*0.5), border);
+    painter.fillRect(QRect(0,0,zoom*(0.5),zoom*(n+1.0)), border);
+    painter.fillRect(QRect(zoom*(n+0.5),0,zoom*0.5,zoom*(n+1.0)), border);
+
+    QColor dark(0xcc,0xcc,0xcc);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            QRect rect = pixelRect(i, j);
+            if((i + j)%2 == 1) {
+                    painter.fillRect(rect, dark);
+            } else {
+                painter.fillRect(rect, Qt::white);
+            }
+        }
+    }
+
+    QColor endColor(0x90,0x00,0x90);
+    QColor startColor(0x33,0xff,0x00);
+    QColor capturedColor(0xff,0x33,0x33);
+    QColor normalColor(0x4c,0x4c,0xcc);
+
+    int s = zoom*0.4;
+    int sd = zoom*0.2;
+    int i = 0, j = 0;
+
+    //выделение
+    if(selectedCelli != -1 && selectedCellj != -1)
+    {
+        QRect rect = pixelRect(selectedCelli, selectedCellj);
+        painter.fillRect(rect, startColor);
+    }
+
+    //белые
+    painter.setPen(QPen(Qt::black,zoom*0.1));
+    painter.setBrush(QBrush(Qt::white));
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            //на нужной клетке
+            if((i + j) % 2 == 1)
+            {
+                if(draughts[i][j] == WHITE)
+                {
+                    painter.drawEllipse(QPoint(zoom * (j + 1), zoom * (i + 1)), s, s);
+                }
+                if(draughts[i][j] == WHITE_KING)
+                {
+                    painter.drawEllipse(QPoint(zoom * (j + 1), zoom * (i + 1)), s, s);
+                    painter.drawEllipse(QPoint(zoom * (j + 1), zoom * (i + 1)), sd, sd);
+                }
+            }
+        }
+    }
+
+    //чёрные
+    painter.setBrush(QBrush(Qt::black));
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            //на нужной клетке
+            if((i + j) % 2 == 1)
+            {
+                if(draughts[i][j] == BLACK)
+                {
+                    painter.drawEllipse(QPoint(zoom * (j + 1), zoom * (i + 1)), s, s);
+                }
+                if(draughts[i][j] == BLACK_KING) {
+                    painter.drawEllipse(QPoint(zoom * (j + 1), zoom * (i + 1)), s, s);
+                    painter.setPen(QPen(Qt::white,zoom*0.1));
+                    painter.drawEllipse(QPoint(zoom * (j + 1), zoom * (i + 1)), sd, sd);
+                    painter.setPen(QPen(Qt::black,zoom*0.1));
+                }
+            }
+        }
+    }
+}
+
+void CheckersPicture::resizeEvent (QResizeEvent * event) {
+    if(event->oldSize()!=event->size()) {
+        update();
+        side = qMin(width(), height());
+        p = QPoint((width() - side) / 2, (height() - side) / 2);
+    } else {
+        event->ignore();
+    }
+}
+
+QRect CheckersPicture::pixelRect(int i, int j) const
+{
+    return QRect(zoom * (j + 0.5), zoom * (i + 0.5), zoom, zoom);
+}
+
+
---
+++ project/poddavki.pl
@@ -0,0 +1,430 @@
+%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!%
+%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Copyright © 2011 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!%
+%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!%
+  
+%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! GLOSSARY !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!%
+% checkers              - шашки (амер.)
+% suicide checkers      - поддавка (also anti-checkers, giveaway checkers)
+% computer              - компьютер
+% player                - игрок
+% checker               - шашка
+% king                  - дамка
+
+%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!%
+:- dynamic 
+	computer_checker/2.
+:- dynamic 
+	player_checker/2.
+:- dynamic 
+	computer_king/2.	
+:- dynamic 
+	player_king/2.
+:- dynamic
+	is_member/2.
+	
+%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! START POSITIONS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!%
+
+computer_checker(3,0).
+
+player_checker(2,7).
+player_checker(4,1).
+player_checker(6,3).
+
+
+%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!%
+% координаты на доске
+onboard(X, Y):-
+   X > -1, X < 8, Y > -1, Y < 8.
+
+% клетка пуста
+empty(X, Y):-
+	onboard(X, Y),
+	not(computer_checker(X, Y)),
+	not(player_checker(X, Y)),
+	not(computer_king(X, Y)),
+	not(player_king(X, Y)),
+	!.
+
+% если можно перейти на клетку (X2, Y2) из (X1, Y1), сместившись на 1 клетку в направлении (Dx, Dy)
+next_cell(X1, Y1, Dx, Dy, X2, Y2):-
+	X2 is X1 + Dx,
+	Y2 is Y1 + Dx,
+	empty(X2, Y2).
+	
+next_cell(X1, Y1, Dx, Dy, X2, Y2):-
+	X is X1 + Dx,
+	Y is Y1 + Dy,
+	empty(X, Y),
+	next_cell(X, Y, Dx, Dy, X2, Y2).
+
+% проверка, есть ли элемент X в большом списке []
+is_member(X,[X,List]).
+is_member(X,[Element|List]):-
+	is_member(X,List).	
+	
+%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! PLAYER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!%
+% сохраняем дамку в БД, если игрок дошел до верху шашкой
+player_try_to_get_king(0, Y2):-
+	player_checker(0, Y2),
+	retract(player_checker(0, Y2)),		    % удаляем шашку
+	assert(player_king(0, Y2)).             % добавляем дамку
+	
+player_try_to_get_king(_, _).
+
+% проверка на то, что шашка игрока должна есть фигуру из (X1, Y1) фигуру (Gx, Gy) перейдя в (X2, Y2)
+player_checker_need_kill(X1, Y1, X2, Y2, Gx, Gy):-
+	computer_checker(Gx, Gy),		% шашка игрока на Gx, Gy
+	Dx is Gx - X1,					% считаем смещение
+	Dy is Gy - Y1,
+	T1 is abs(Dx), 
+	T1 = 1,						    % проверяем, что расстояние по х...
+	T2 is abs(Dy), 
+	T2 = 1,						    % и y == 1
+	X2 is Gx + Dx,					% определяем позицию, 
+	Y2 is Gy + Dy,					% в которую должна попасть шашка после съедания
+	empty(X2, Y2).					% удостоверяемся, что там пусто
+
+% съесть дамкой из (X1, Y1) фигуру (Gx, Gy) перейдя в (X2, Y2)
+player_king_need_kill(X1, Y1, X2, Y2, Gx, Gy):-
+	computer_king(Gx,Gy),
+	Dx is Gx - X1,
+	Dy is Gy - Y1,
+	T1 is abs(Dx),
+	T2 is abs(Dy), 
+	T2 = T1,			            % смещение для дамки(х == у)
+	next_cell(Gx, Gy, Dx, Dy, X2, Y2),
+	onboard(X2, Y2).
+
+% наличие шашки, которой можно бить
+player_checker_can_kill(X, Y, Killed):-
+	player_checker_need_kill(X, Y, _, _, Gx, Gy),
+	not(is_member([Gx, Gy], Killed)),
+	!.
+
+% наличие дамки, которой можно бить
+player_king_can_kill(X, Y, Killed):-
+	player_king_need_kill(X, Y, _, _, Gx, Gy),
+	not(is_member([Gx, Gy], Killed)),
+	!.
+
+% удаление всех съеденых фигур из БД
+player_remove_all([]):-
+	!.
+	
+player_remove_all([[X, Y] | T]):-
+	(retract(computer_checker(X, Y)) ; retract(computer_king(X, Y))),
+	player_remove_all(T).
+
+% съедать шашкой пока не наестся
+player_checker_can_continue(X2, Y2, X2, Y2, L):-
+	not(player_checker_can_kill(X2, Y2, L)),
+	player_remove_all(L),
+	!.
+	
+player_checker_can_continue(X1, Y1, X2, Y2, Killed):-
+	player_checker_need_kill(X1, Y1, Ex1, Ey1, Gx, Gy),
+	not(is_member([Gx, Gy], Killed)),
+	player_checker_can_continue(Ex1, Ey1, X2, Y2, [[Gx, Gy] | Killed]).
+
+% съедать дамкой пока не наестся
+player_king_can_continue(X2, Y2, X2, Y2, L):-
+	not(player_king_can_kill(X2, Y2, L)),
+	player_remove_all(L),
+	!.
+	
+player_king_can_continue(X1,Y1,X2,Y2,Killed):-
+	player_king_need_kill(X1, Y1, Ex1, Ey1, Gx, Gy),
+	not(is_member([Gx, Gy], Killed)),
+	player_king_can_continue(Ex1, Ey1, X2, Y2, [[Gx, Gy] | Killed]).
+
+% у игрока есть ход шашкой из (X1, Y1) в (X2, Y2)
+player_move(X1, Y1, X2, Y2):-
+	player_checker(X1, Y1),
+	player_checker_need_kill(X1, Y1, Ex1, Ey1, Gx, Gy),
+	player_checker_can_continue(Ex1, Ey1, X2, Y2, [[Gx, Gy]]),
+	retract(player_checker(X1, Y1)),
+	assert(player_checker(X2, Y2)),
+	player_try_to_get_king(X2,Y2).
+
+player_move(X1, Y1, X2, Y2):-
+	player_checker(X1, Y1),
+	not(player_need_to_kill),
+	empty(X2, Y2),
+	T1 is X2 - X1, 
+	T1 = -1,
+	T2 is abs(Y2 - Y1),
+	T2 = 1,
+	onboard(X2, Y2),
+	retract(player_checker(X1, Y1)),
+	assert(player_checker(X2, Y2)),
+	player_try_to_get_king(X2, Y2).
+
+player_move(X1, Y1, X2, Y2):-
+	player_king(X1, Y1),
+	player_king_need_kill(X1, Y1, Ex1, Ey1, Gx, Gy),
+	player_king_can_continue(Ex1, Ey1, X2, Y2, [[Gx, Gy]]),
+	retract(player_king(X1, Y1)),
+	assert(player_king(X2, Y2)).
+
+% ход дамкой
+player_move(X1, Y1, X2, Y2):-
+	player_king(X1, Y1),			    % дамка
+	not(player_need_to_kill),		    % не надо никого съедать
+	empty(X2, Y2),					    % в клетке пусто
+	T1 is abs(X2 - X1),		            % cмещение по х
+	T2 is abs(Y2 - Y1),	                % и у
+	T1 = T2,						    % равное
+	onboard(X2, Y2),				    % Клетка на столе(опять же, кажется, что не там, где надо)
+	retract(player_king(X1, Y1)),	    % Удалить данные о дамке
+	assert(player_king(X2, Y2)).	    % Записать данные о дамке
+
+% игрок может съесть кого-нибудь
+player_need_to_kill:-
+	(player_checker(X, Y), player_checker_can_kill(X, Y, []) ; (player_king(X, Y), player_king_can_kill(X, Y, []))).
+	
+% определение направлений ходов (X2, Y2) для фигуры из (X1, Y1)
+player_can_move(X1, Y1, X2, Y2):-     % шашка в 2х направлениях
+	player_checker(X1, Y1),
+	X2 is X1 - 1,
+	Y2 is Y1 - 1.
+player_can_move(X1, Y1, X2, Y2):-
+	player_checker(X1, Y1),
+	X2 is X1 - 1,
+	Y2 is Y1 + 1.
+player_can_move(X1, Y1, X2, Y2):-     % дамка в 4х направлениях 
+	player_king(X1, Y1),
+	X2 is X1 + 1,
+	Y2 is Y1 + 1.
+player_can_move(X1, Y1, X2, Y2):-
+	player_king(X1, Y1),
+	X2 is X1 + 1,
+	Y2 is Y1 - 1.
+player_can_move(X1, Y1, X2, Y2):-
+	player_king(X1, Y1), 
+	X2 is X1 - 1,
+	Y2 is Y1 + 1.
+player_can_move(X1, Y1, X2, Y2):-
+	player_king(X1, Y1),
+	X2 is X1 - 1,
+	Y2 is Y1 - 1.
+	
+player_try_move(X1, Y1):-
+	player_checker(X1,Y1),
+	player_checker_need_kill(X1, Y1, X2, Y2, Px1, Py1),
+	!.
+player_try_move(X1, Y1):-
+	player_king(X1, Y1),
+	player_king_need_kill(X1, Y1, X2, Y2, Px1, Py1),
+	!.
+player_try_move(X1, Y1):-
+	player_checker(X1, Y1),
+	player_can_move(X1, Y1, X2, Y2),
+	empty(X2, Y2).	
+
+%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! AI !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!%
+% компьютер может бить, ходя из (X1, Y1) в (X2, Y2), съедая (Gx, Gy)
+computer_checker_need_kill(X1, Y1, X2, Y2, Gx, Gy):-
+	player_checker(Gx, Gy),
+	Dx is Gx - X1,
+	Dy is Gy - Y1,
+	T1 is abs(Dx), 
+	T1 = 1,	                    % проверка, находится ли вражеская шашка рядом
+	T2 is abs(Dy), 
+	T2 = 1,
+	X2 is Gx + Dx,			    % X2 вычисляется как смещение на Dx
+	Y2 is Gy + Dy, 		        % Y2 вычисляется как смещение на Dy
+	empty(X2, Y2).
+
+% съедать шашкой пока не наестся
+computer_checker_can_continue(X2, Y2, X2, Y2, L):-
+	not(computer_checker_can_kill(X2, Y2, L)),
+	computer_remove_all(L),
+	!.
+	
+computer_checker_can_continue(X1, Y1, X2, Y2, Killed):-
+	computer_checker_need_kill(X1, Y1, Ex1, Ey1, Gx, Gy),
+	not(is_member([Gx, Gy], Killed)),					% проверка на то, что элемента Gx, Gy нет в списке убитых
+	computer_checker_can_continue(Ex1, Ey1, X2, Y2, [[Gx, Gy] | Killed]).
+
+% шашке компьютера нужно бить
+computer_checker_can_kill(X, Y, Killed):-
+	computer_checker_need_kill(X, Y, _, _, Gx, Gy),		% определяем координаты шашки, которую нужно бить
+	not(is_member([Gx, Gy], Killed)),			        % проверяем, что она не находится в списке убитых
+	!.
+
+% определение направлений ходов (X2, Y2) для фигуры из (X1, Y1)
+computer_can_move(X1, Y1, X2, Y2):-     % шашка в 2х направлениях
+	computer_checker(X1, Y1),
+	X2 is X1 + 1,
+	Y2 is Y1 - 1.
+computer_can_move(X1, Y1, X2, Y2):-
+	computer_checker(X1, Y1),
+	X2 is X1 + 1,
+	Y2 is Y1 + 1.
+computer_can_move(X1, Y1, X2, Y2):-     % дамка в 4х направлениях 
+	computer_king(X1, Y1),
+	X2 is X1 + 1,
+	Y2 is Y1 + 1.
+computer_can_move(X1, Y1, X2, Y2):-
+	computer_king(X1, Y1),
+	X2 is X1 + 1,
+	Y2 is Y1 - 1.
+computer_can_move(X1, Y1, X2, Y2):-
+	computer_king(X1, Y1), 
+	X2 is X1 - 1,
+	Y2 is Y1 + 1.
+computer_can_move(X1, Y1, X2, Y2):-
+	computer_king(X1, Y1),
+	X2 is X1 - 1,
+	Y2 is Y1 - 1.
+
+% вычисление хода шашкой из (X1, Y1) в (X2, Y2) такого, что игроку придется съесть
+computer_checker_next_step_player_can_to_kill(X1, Y1, X2, Y2):-
+	retract(computer_checker(X1, Y1)),
+	assert(computer_checker(X2, Y2)),
+	player_need_to_kill,
+	retract(computer_checker(X2, Y2)),
+	assert(computer_checker(X1, Y1)),
+	!.
+	
+computer_checker_next_step_player_can_to_kill(X1, Y1, X2, Y2):-
+	retract(computer_checker(X2, Y2)),
+	assert(computer_checker(X1, Y1)),
+	!,
+	fail.
+
+% вычисление хода дамкой из (X1, Y1) в (X2, Y2) такого, что игроку придется съесть
+computer_king_next_step_player_can_to_kill(X1, Y1, X2, Y2):-
+	retract(computer_king(X1, Y1)),
+	assert(computer_king(X2, Y2)),
+	player_need_to_kill,
+	retract(computer_king(X2, Y2)),
+	assert(computer_king(X1, Y1)),
+	!.
+
+computer_king_next_step_player_can_to_kill(X1, Y1, X2, Y2):-
+	retract(computer_king(X2, Y2)),
+	assert(computer_king(X1, Y1)),
+	!,
+	fail.
+
+% сохраняем дамку в БД, если компьютер дошел до низа шашкой
+computer_try_to_get_king(7, Y2):-
+	computer_checker(7, Y2),
+	retract(computer_checker(7, Y2)),
+	assert(computer_king(7, Y2)).
+computer_try_to_get_king(_, _).
+
+% съесть дамкой из (X1, Y1) фигуру (Gx, Gy) перейдя в (X2, Y2)
+computer_king_need_kill(X1, Y1, X2, Y2, Gx, Gy):-
+	player_king(Gx, Gy),
+	Dx is Gx - X1,
+	Dy is Gy - Y1,
+	T1 is abs(Dx),
+	T2 is abs(Dy),
+	T2 = T1,
+	next_cell(Gx, Gy, Dx, Dy, X2, Y2),
+	onboard(X2, Y2).
+
+% съедать дамкой пока не наестся
+computer_king_can_continue(X2, Y2, X2, Y2, L):-
+	not(computer_king_can_kill(X2, Y2, L)),
+	computer_remove_all(L),
+	!.
+	
+computer_king_can_continue(X1, Y1, X2, Y2, Killed):-
+	computer_king_need_kill(X1, Y1, Ex1, Ey1, Gx, Gy),
+	not(is_member([Gx, Gy], Killed)),
+	computer_king_can_continue(Ex1, Ey1, X2, Y2, [[Gx, Gy1] | Killed]).
+
+% дамкой может съесть
+computer_king_can_kill(X, Y, Killed):-
+	computer_king_need_kill(X, Y, _, _, Gx, Gy),
+	not(is_member([Gx, Gy], Killed)),
+	!.
+
+computer_try_move(X1, Y1):-
+	computer_checker(X1,Y1),
+	computer_checker_need_kill(X1, Y1, X2, Y2, Px1, Py1),
+	!.
+computer_try_move(X1, Y1):-
+	computer_king(X1, Y1),
+	computer_king_need_kill(X1, Y1, X2, Y2, Px1, Py1),
+	!.
+computer_try_move(X1, Y1):-
+	computer_checker(X1, Y1),
+	computer_can_move(X1, Y1, X2, Y2),
+	empty(X2, Y2).
+	
+% шашка убивает
+computer_move:-
+	computer_checker(X1,Y1),		                % на (X1, Y1) шашка
+	computer_checker_need_kill(X1, Y1, X2, Y2, Px1, Py1),
+	computer_checker_can_continue(X2, Y2, Xr, Yr, [[Px1, Py1]]),
+	retract(computer_checker(X1,Y1)),		        % удаление данных о предыдущем положении шашки компьютера
+	assert(computer_checker(Xr,Yr)),		        % запись данных о новом положении шашки компьютера
+	computer_try_to_get_king(Xr,Yr),			    % проверка на дамку
+	!.		  	
+
+% дамка убивает
+computer_move:-
+	computer_king(X1,Y1),
+	computer_king_need_kill(X1, Y1, X2, Y2, Px1, Py1),
+	computer_king_can_continue(X2, Y2, Xr, Yr, [[Px1, Py1]]),
+	retract(computer_king(X1, Y1)),
+	assert(computer_king(Xr, Yr)),
+	!.
+
+% дамка умно ходит
+computer_move:-
+	computer_king(X1, Y1),
+	computer_can_move(X1, Y1, X21, Y21),
+	empty(X2, Y2),        
+	computer_king_next_step_player_can_to_kill(X1, Y1, X2, Y2),
+	retract(computer_king(X1, Y1)),
+	assert(computer_king(X2, Y2)),
+	!.
+
+% шашка ходит из X1 Y1 в X2 Y2
+computer_move:-
+	computer_checker(X1, Y1),			    % шашка компьютера на Х1, У1
+	computer_can_move(X1, Y1, X2, Y2),	    % проверка на ходы(для шашек x is y is 1, для дамок x is y)
+	empty(X2, Y2),						    % клетка X2, Y2 пуста
+	computer_checker_next_step_player_can_to_kill(X1, Y1, X2, Y2),
+	retract(computer_checker(X1, Y1)),	    % удаление данных о предущей позиции шашки
+	assert(computer_checker(X2, Y2)),	    % запись новой позиции
+	computer_try_to_get_king(X2, Y2).
+
+% шашка просто ходит, так как пока нету шанса, чтобы после хода компьютера игрок съел фигуру 
+computer_move:-
+	computer_checker(X1, Y1),
+	computer_can_move(X1, Y1, X2, Y2),
+	empty(X2, Y2),
+	retract(computer_checker(X1, Y1)),
+	assert(computer_checker(X2, Y2)),
+	computer_try_to_get_king(X2,Y2).
+
+% дамка просто ходит, так как пока нету шанса, чтобы после хода компьютера игрок съел фигуру 
+computer_move:-
+	computer_king(X1, Y1),
+	computer_can_move(X1, Y1, X2, Y2),
+	empty(X2, Y2),
+	retract(computer_king(X1, Y1)),
+	assert(computer_king(X2, Y2)).
+	
+% удаление всех съеденых фигур из БД
+computer_remove_all([]):-
+	!.
+	
+computer_remove_all([[X, Y] | T]):-
+	(retract(player_checker(X, Y)) ; retract(player_king(X, Y))),
+	computer_remove_all(T).
+
+%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! WIN !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!%
+% условия победы
+computer_win:-
+	not(computer_checker(_, _));
+	(computer_checker(_, _), not(computer_try_move(_, _))).
+player_win:-
+	not(player_checker(_, _));
+	(player_checker(_, _), not(player_try_move(_, _))).
